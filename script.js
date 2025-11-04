aa259d14f98b5970ffced62aaf5aaa7bc26258cf
Richie <mondragon.richie@gmail.com>
2025-11-04 12:33:33 -0800

    Initial commit: Doctor Website trials

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+document.getElementById("helloBtn").addEventListener("click", () => {
+  alert("You clicked the button!");
+});
